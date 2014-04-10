32230156d6fa815553e539f3950c25c1cd7cf551
Schell Scivally <efsubenovex@gmail.com>
2014-04-10 08:22:09 -0700

    new project

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,113 @@
 module Main where
 
-import Voxy
-import System.Console.GetOpt
-import System.Environment
-import System.FilePath          ( (</>) )
+import           Urza
+import qualified Urza.Color as Color
+import           Urza.Shader
+import           Foreign
+import           Control.Concurrent.MVar
+import           Graphics.Rendering.OpenGL hiding (Matrix, renderer, get, drawPixels, Bitmap)
+import           Control.Lens hiding ((#))
+import           Control.Monad
+import           System.Directory
+import           System.FilePath ((</>))
+import           System.Exit
+
+
 
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case getOpt Permute options args of
-        (  [], _,   []) -> error $ usageInfo header options
-        (opts, _,   []) -> startWithOpts $ foldl (flip id) defaultOptions opts
-        (   _, _, msgs) -> error $ concat msgs ++ usageInfo header options
+    let txt = "Hey there!\nHello."
 
-header :: String
-header = "Usage: voxy -d data_dir"
+    mvar <- newMVar 0
+    wvar <- initUrza (100,100) (800,600) "Voxy Town"
+    fontDir <- fmap (</> "assets" </> "font") getCurrentDirectory
+    imgDir  <- fmap (</> "assets" </> "img") getCurrentDirectory
 
-data Options = Options { optDataDir :: FilePath } deriving (Show, Eq)
+    r <- loadText txt =<< makeRenderer (fontDir </> "Deutsch.ttf") 128
+    Just (bmp@(Bitmap t size)) <- loadBitmap (imgDir </> "oryx_roguelike_16x24.png")
 
-defaultOptions :: Options
-defaultOptions = Options { optDataDir = "." </> "data" }
+    let vs = [ -1,  1, -1 -- Front face
+             ,  1,  1, -1
+             , -1, -1, -1
+             ,  1, -1, -1
+             , -1,  1,  1 -- Back face
+             ,  1,  1,  1
+             , -1, -1,  1
+             ,  1, -1,  1
+             ]
+        --cs = concat $ replicate 4 [ 1,1,1,1 ]
+        cs = [ 0, 1, 0, 1
+             , 1, 1, 0, 1
+             , 0, 0, 0, 1
+             , 1, 0, 0, 1
 
-options :: [OptDescr (Options -> Options)]
-options = [ Option "d" ["data directory"]
-              (ReqArg (\d opts -> opts { optDataDir = d }) "DATA_DIRECTORY")
-              "set the directory to read/write data from"
-          ]
+             , 0, 1, 1, 1
+             , 1, 1, 1, 1
+             , 0, 0, 1, 1
+             , 1, 0, 1, 1
+             ]
+        ndxs = [ 0, 1, 2
+               , 1, 2, 3
 
-startWithOpts :: Options -> IO ()
-startWithOpts opts = voxy $ optDataDir opts 
+               , 4, 5, 6
+               , 5, 6, 7
+
+               , 4, 5, 0
+               , 5, 0, 1
+
+               , 6, 7, 2
+               , 7, 2, 3
+
+               , 1, 5, 3
+               , 5, 3, 7
+
+               , 4, 0, 6
+               , 0, 6, 2
+               ] :: [GLubyte]
+
+
+        tn = translationMatrix3d 100 100 0
+        s  = scaleMatrix3d 10 10 10
+        rt = rotationMatrix3d 0 0 0
+        mv = identityN 4 --`multiply` s --rt `multiply` s `multiply` tn
+
+    r^.shader.setIs3d $ True
+    r^.shader.setIsTextured $ False
+    r^.shader.setColorIsReplaced $ False
+    clientState VertexArray $= Enabled 
+
+    --print vs
+    i <- genObjectName
+    bindBuffer ElementArrayBuffer $= Just i
+    withArray ndxs $ \ptr -> do
+        bufferData ElementArrayBuffer $= (sizeOfUByteList ndxs, ptr, StaticDraw)
+    (j,k) <- bindAndBufferVerts3Colors vs cs
+
+
+    forever $ do
+        pollEvents
+        (_, window) <- takeMVar wvar
+        (winW, winH) <- fmap (over both fromIntegral) $ getWindowSize window
+
+        makeContextCurrent $ Just window
+        viewport $= (Position 0 0, Size winW winH)
+        clearColor $= Color.black
+        clear [ColorBuffer, DepthBuffer]
+
+        r^.shader.setProjection $ concat $ orthoMatrix (-10) 10 10 (-10) (-10) 10
+        rads <- takeMVar mvar
+        let mv' = mv `multiply` rotationMatrix3d rads rads rads
+        putMVar mvar $ rads + 0.01
+        r^.shader.setModelview $ concat mv'
+        drawElements Triangles (fromIntegral $ length ndxs) UnsignedByte nullPtr
+
+        --drawElements Triangles 3 UnsignedInt nullPtr
+        --drawArrays Triangles 0 6
+
+        -- Render the display list.
+--        modifyMVar_ sceneVar $ renderScene (Size winW winH)
+        swapBuffers window
+        shouldClose <- windowShouldClose window
+        putMVar wvar ([],window)
+        when shouldClose exitSuccess
--- src/Util.hs
+++
@@ -1,7 +0,0 @@
-module Util where
-
-import Graphics.Rendering.OpenGL
-import System.IO                    (hPutStrLn, stderr)
-
-printError :: IO ()
-printError = get errors >>= mapM_ (hPutStrLn stderr . ("GL: "++) . show)

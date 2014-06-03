7ba985d45daddadba8c5960729ec0fb8191348b6
Schell Scivally <efsubenovex@gmail.com>
2014-06-02 18:51:19 -0700

    cemented time

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Arrows #-}
 module Main where
 
 import           Prelude hiding ((.), id, until)
 import           Urza
-import           Urza.Wire
-import           Tween
 import           FRP.Netwire
 import           Control.Monad.Reader hiding (when)
 import qualified Urza.Color as Color
@@ -17,24 +16,31 @@ import           System.Directory
 import           System.FilePath ((</>))
 import           System.Exit
 import           Linear hiding (trace)
+import           Debug.Trace
 
 
 -- | TODO: Get rid of the many MVars.
 
 main :: IO ()
 main = do
-    let txt = map toEnum [32..126]
-
     wvar    <- initUrza (100,100) (800,600) "Wirez"
-    evar    <- newMVar def 
-    wirevar <- newMVar (clockSession_, textAndPosWire, Right ("...", Position 0 0))
     fontDir <- fmap (</> "Library" </> "Fonts") getHomeDirectory
     imgDir  <- fmap (</> "assets" </> "img") getCurrentDirectory
+    r       <- makeAsciiRenderer (fontDir </> "UbuntuMono-R.ttf") 24
 
-    r <- loadText txt =<< makeRenderer (fontDir </> "UbuntuMono-R.ttf") 24
-    --Just graph <- graphFromFile (imgDir </> "oryx_roguelike_16x24.png")
-    Just bmp <- loadBitmap (imgDir </> "photo.png")
-    Just face <- loadBitmap (imgDir </> "face.png")
+
+    let renderFace :: Either () Bitmap_Transform2d -> IO (Either () Bitmap_Transform2d)
+        renderFace (Left ()) = do
+            Just face <- loadBitmap (imgDir </> "face.png")
+            return $ Right (face, def{ _t2Size = trace (show $ face^.bitmapSize) face^.bitmapSize})
+        renderFace (Right (bmp, tfrm)) = do
+            drawBitmap r bmp tfrm
+            return $ Right (bmp, tfrm)
+
+    ivar <- newMVar $ def { _iRender = renderFace
+                          , _iSession = clockSession_
+                          , _iWire = myWire
+                          }
 
     M.forever $ do
         -- Execute Urza callbacks and load up events.
@@ -47,15 +53,7 @@ main = do
         -- Put the rest back for later.
         putMVar wvar (events', window)
 
-        -- Process, update and return our environment.
-        env <- processEnv mEvent <$> takeMVar evar
-        putMVar evar env
-
-        (session, wire, _) <- takeMVar wirevar
-        (session', wire', mx) <- step session wire env
-        putMVar wirevar (session', wire', mx)
-
-        -- Render!
+        -- Pre render setup
         (winW, winH) <- fmap (over both fromIntegral) $ getWindowSize window
         makeContextCurrent $ Just window
         viewport $= (Position 0 0, Size winW winH)
@@ -68,51 +66,38 @@ main = do
 
         r^.shader.setProjection $ orthoM44 0 (fromIntegral winW) 0 (fromIntegral winH) 0 1
         r^.shader.setModelview $ eye4
-        r^.shader.setTextColor $ Color4 1 1 0 1
-        r^.shader.setColorIsReplaced $ True
 
-        case mx of
-            Left _ -> putStrLn $ "Inhibited"
-            Right (text, pos) -> do
-                M.void $ drawTextAt' r pos text
-        drawBitmap r face (Position 0 50) (face^.bitmapSize) (Scale 1 1) (Rotation 0)
+        -- Process, update and render our app iteration.
+        stepAndRender ivar mEvent
 
         swapBuffers window
         shouldClose <- windowShouldClose window
         M.when shouldClose exitSuccess
 
---myWire :: (MonadReader Env m, HasTime t s, Monoid e, Fractional t) => Wire s e m a String
---myWire = whenCursorIsOnScreen . cursorPositionStartingWith (Position 0 0) <|> pure (Position 0 0)
+myWire :: (MonadReader Env m, Monoid e) => Wire TimeDelta e m Bitmap_Transform2d Bitmap_Transform2d
+myWire = proc (bmp, tfrm) -> do
+    pos <- tweenPosWire -< ()
+    returnA -< (bmp, tfrm & t2Position .~ pos)
 
-data Transform2d = Transform2d { _t2Position :: Position
-                               , _t2Size     :: Size
-                               , _t2Scale    :: Scale
-                               , _t2Rotation :: Rotation
-                               } deriving (Show)
 
-data Renderable2d a = Renderable2d { _rTransform :: Transform2d
-                                   , _rData      :: a
-                                   , _rRender    :: a -> IO ()
-                                   }
+textAndPosWire :: (MonadReader Env m, Monoid e) => Wire TimeDelta e m a (String, Position)
+textAndPosWire = (,) <$> stringWire "" <*> posWire
 
-textAndPosWire :: (MonadReader Env m, HasTime t s, Monoid e, Fractional t) => Wire s e m a (String, Position)
-textAndPosWire = (,) <$> stringWire "" <*> posWire 
+textWire :: (MonadReader Env m, Monoid e) => Wire TimeDelta e m a String
+textWire = ("Window size: " ++) . show <$> asSoonAs . windowResizeEvent
 
-textWire :: (MonadReader Env m, HasTime t s, Monoid e, Fractional t) => Wire s e m a String
-textWire = ("Window size: " ++) . show <$> asSoonAs . windowResizeEvent 
-
-stringWire :: (MonadReader Env m, HasTime t s, Monoid e, Fractional t) => String -> Wire s e m a String
+stringWire :: (MonadReader Env m, Monoid e) => String -> Wire TimeDelta e m a String
 stringWire str = asSoonAs . accumE (\b a -> b ++ [a]) str . charEvent
 
-posWire :: (MonadReader Env m, HasTime t s, Monoid e, Fractional t) => Wire s e m a Position
+posWire :: (MonadReader Env m, Monoid e) => Wire TimeDelta e m a Position
 posWire = cursor2Pos . asSoonAs . mouseButtonEvent MouseButton'1 MouseButtonState'Pressed
 
 -- Animate back and forth horizontally.
-tweenPosWire :: (Monad m, Monoid e, HasTime t s, Fractional t) => Wire s e m a Position
+tweenPosWire :: (Monad m, Monoid e) => Wire TimeDelta e m a Position
 tweenPosWire = Position <$> tween <*> tween
     where tween1 = for 1 . fmap round (easeInOutExpo (0 :: Double) 200 1)
           tween2 = for 1 . fmap round (easeInOutExpo (200 :: Double) 0 1)
           tween  = tween1 --> tween2 --> tween
 
-cursor2Pos :: (Monad m) => Wire s e m (Double, Double) Position
+cursor2Pos :: (Monad m) => Wire TimeDelta e m (Double, Double) Position
 cursor2Pos = arr $ \(x, y) -> Position (round x) (round y)
--- src/Types.hs
+++
@@ -1,9 +0,0 @@
-module Types where
-
-import           Urza
-import           FRP.Netwire
-import           Control.Monad.Reader
-
-type Step s w b = (s, w, b)
-
-type UrzaWire s b = Wire s () (ReaderT Env IO) () b

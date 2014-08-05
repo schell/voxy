88b8dcd991900357a910913e0a287397e413298a
Schell Scivally <efsubenovex@gmail.com>
2014-08-04 22:52:03 -0700

    87 line FRP system!

--- src/Wire.hs
+++ src/Wire.hs
@@ -1,109 +1,87 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Data.Time.Clock
 import Control.Applicative
-import Control.Concurrent
---import Control.Monad.Reader
+import Control.Monad
+import Control.Monad.Identity
 import System.Exit
 import System.Environment
 
 main :: IO ()
 main = do
     s:_  <- getArgs
-    v    <- newMVar $ read s
     t    <- getCurrentTime
-    loop v t $ MyRecord <$> timeVaryingInteger <*> timeVaryingString
+    let timeVaryingInt = (constWire 45 ~> at 3) `startingWith` 666
+        timeVaryingStr = (constWire "." ~> at 4) `startingWith` "Running..."
+    loop (read s) t $ (,) <$> timeVaryingInt <*> timeVaryingStr
 
-loop :: Show b => MVar Double -> UTCTime -> Wire () b -> IO ()
-loop v t w = do
+loop :: Time -> UTCTime -> Wire Identity () (Int, String) -> IO ()
+loop tleft t w = do
     t' <- getCurrentTime
     let dt = realToFrac $ diffUTCTime t' t
-    let Output t'' w' = stepWire w dt ()
-    print t''
-    tleft <- takeMVar v
+        Identity (Output val w') = stepWire w dt ()
+    print val
     let tleft' = tleft - dt
     if tleft' <= 0
       then exitSuccess
-      else do putMVar v tleft'
-              loop v t' w'
+      else loop tleft' t' w'
 
---isZero :: Wire () Bool
---isZero = Wire $ \dt a -> do
---    e <- ask
---    return $ if e == 0
---               then Output True isZero
---               else Output False isZero
+(~>) :: Monad m => Wire m a b -> Wire m b c -> Wire m a c
+(~>) w1 w2 = Wire $ \dt a -> do Output !b w1' <- stepWire w1 dt a
+                                Output !c w2' <- stepWire w2 dt b
+                                return $ Output c $ w1' ~> w2'
 
-timeVaryingInteger :: Wire () Integer
-timeVaryingInteger = (simply 45 ~> at 3) `startingWith` 666
-
-timeVaryingString :: Wire () String
-timeVaryingString = (simply "Done." ~> at 4) `startingWith` "Almost done."
-
-(~>) :: Wire a b -> Wire b c -> Wire a c
-(~>) = plug
-
-plug :: Wire a b -> Wire b c -> Wire a c
-plug w1 w2 = Wire $ \dt a ->
-    let Output b w1' = stepWire w1 dt a
-        Output c w2' = stepWire w2 dt b
-    in Output c $ w1' `plug` w2'
-
-startingWith :: Wire a (Maybe b) -> b -> Wire a b
+startingWith :: Monad m => Wire m a (Maybe b) -> b -> Wire m a b
 startingWith = holdWith
 
-holdWith :: Wire a (Maybe b) -> b -> Wire a b
-holdWith w def = Wire $ \dt x ->
-    let Output mb w' = stepWire w dt x in
-    case mb of
-        Nothing -> Output def $ holdWith w' def
-        Just b  -> Output b $ holdWith w' b
+holdWith :: Monad m => Wire m a (Maybe b) -> b -> Wire m a b
+holdWith w def = Wire $ \dt x -> do Output mb w' <- stepWire w dt x
+                                    return $ case mb of
+                                        Nothing -> Output def $ holdWith w' def
+                                        Just !b -> Output b $ holdWith w' b
 
-at :: Time -> Wire a (Maybe a)
+at :: Monad m => Time -> Wire m a (Maybe a)
 at t = Wire $ \dt x ->
     let !t' = t - dt in
-    if t' <= 0
-      then Output (Just x) (mkConst Nothing)
+    return $ if t' <= 0
+      then Output (Just x) (constWire Nothing)
       else Output Nothing (at t')
 
-time :: Wire a Time
+time :: Monad m => Wire m a Time
 time = timeFrom 0
 
-timeFrom :: Double -> Wire a Time
+timeFrom :: Monad m => Double -> Wire m a Time
 timeFrom t = Wire $ \dt _ ->
     let t' = t + dt in
-    Output t' (timeFrom t')
+    return $ Output t' (timeFrom t')
 
-stepWire :: Wire a b -> Time -> a -> Output a b
+instance Monad m => Applicative (Wire m a) where
+    pure = constWire
+    wf <*> wa = Wire $ \dt a -> do Output !f wf' <- stepWire wf dt a
+                                   Output !b wa' <- stepWire wa dt a
+                                   return $ Output (f b) (wf' <*> wa')
+
+constWire :: Monad m => b -> Wire m a b
+constWire b = pureWire $ \_ _ -> b
+
+pureWire :: Monad m => (Time -> a -> b) -> Wire m a b
+pureWire f = Wire $ \dt a -> return $ Output (f dt a) (pureWire f)
+
+stepWire :: Monad m => Wire m a b -> Time -> a -> m (Output m a b)
 stepWire (Wire !w) dt !a = w dt a
 
-data MyRecord = MyRecord { myInteger :: Integer
-                         , myString  :: String
-                         } deriving (Show)
+instance Monad m => Functor (Wire m a) where
+    fmap f (Wire w) = Wire $ \dt a -> liftM (fmap f) $ w dt a
 
-instance Applicative (Wire a) where
-    pure = simply
-    wf <*> wa = Wire $ \dt a -> let Output a' wa' = stepWire wa dt a
-                                    Output f wf'  = stepWire wf dt a
-                                in Output (f a') (wf' <*> wa')
+instance Monad m => Functor (Output m a) where
+    fmap f (Output val w) = Output (f val) (fmap f w)
 
-simply :: b -> Wire a b
-simply = mkConst
+data Output m a b = Output { outVal  :: !b
+                           , outWire :: !(Wire m a b)
+                           }
 
-mkConst :: b -> Wire a b
-mkConst b = Wire $ \_ _ -> Output b (mkConst b)
-
-instance Functor (Wire a) where
-    fmap f w@(Wire _) = Wire $ \dt a -> let Output b w' = stepWire w dt a
-                                        in Output (f b) (fmap f w')
-
-
-data Output a b = Output { outVal  :: !b
-                         , outWire :: !(Wire a b)
-                         }
-
-data Wire a b = Wire (Time -> a -> Output a b)
+data Wire m a b = Wire (Time -> a -> m (Output m a b))
 
 type Time = Double
-
